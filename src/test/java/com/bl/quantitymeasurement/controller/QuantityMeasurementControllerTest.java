258e42cecfadb40f2d6a50a0dda12aeef156a1a0
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-07-08 01:33:26 +0530

    ADDED: Test For Controller Converting ML To Gallon

--- src/test/java/com/bl/quantitymeasurement/controller/QuantityMeasurementControllerTest.java
+++ src/test/java/com/bl/quantitymeasurement/controller/QuantityMeasurementControllerTest.java
@@ -350,4 +350,22 @@ public class QuantityMeasurementControllerTest {
         String content = result.getResponse().getContentAsString();
         Assert.assertEquals(response,content);
     }
+
+    @Test
+    public void givenQuantityMeasurement_WhenThousandMLConvertToGallon_ThenReturnThreePointSeventyEightResponse() throws Exception {
+        Quantity quantity = new Quantity();
+        quantity.setQuantity(1000);
+        quantity.setFirstSubUnit(UnitConversion.Millilitre);
+        quantity.setSecondSubUnit(UnitConversion.Gallon);
+        String response = mapper.writeValueAsString(new Response("Unit Converted Successfully", 3.78));
+        Mockito.when(quantityMeasurementServiceImpl.unitConversion(Mockito.any())).thenReturn(3.78);
+        String data = mapper.writeValueAsString(quantity);
+        MvcResult result = mockMvc.perform(post("/quantity/convertSubUnitsNames")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(data))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+        Assert.assertEquals(response,content);
+    }
 }

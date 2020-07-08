d259575e212a70d31b784c0a55ffd5923d272dcb
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-07-08 22:43:49 +0530

    ADDED: Test For Service Converting Tonne To GM

--- src/test/java/com/bl/quantitymeasurement/service/QuantityMeasurementServiceImplTest.java
+++ src/test/java/com/bl/quantitymeasurement/service/QuantityMeasurementServiceImplTest.java
@@ -181,6 +181,16 @@ public class QuantityMeasurementServiceImplTest {
     }
 
     @Test
+    public void givenQuantityMeasurement_WhenZeroPointZeroZeroOneTonneConvertToGM_ThenReturnThousand() throws Exception {
+        Quantity quantity = new Quantity();
+        quantity.setQuantity(0.001);
+        quantity.setFirstSubUnit(UnitConversion.Tonne);
+        quantity.setSecondSubUnit(UnitConversion.Gram);
+        double result = quantityMeasurementService.unitConversion(quantity);
+        Assert.assertEquals(1000.0,result,0.0);
+    }
+
+    @Test
     public void givenQuantityMeasurement_WhenHundredCelsiusConvertIntoFahrenheit_ThenReturnTwoHundredTwelve() throws QuantityMeasurementException {
         Quantity quantity = new Quantity();
         quantity.setQuantity(100);

2bace54ff9e53561d924b2f94a297973a6099445
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-07-08 22:47:55 +0530

    ADDED: Test For Service Converting Litre To Gallon

--- src/test/java/com/bl/quantitymeasurement/service/QuantityMeasurementServiceImplTest.java
+++ src/test/java/com/bl/quantitymeasurement/service/QuantityMeasurementServiceImplTest.java
@@ -201,6 +201,16 @@ public class QuantityMeasurementServiceImplTest {
     }
 
     @Test
+    public void givenQuantityMeasurement_WhenThreePointSeventyEightLitreConvertToGallon_ThenReturnOne() throws Exception {
+        Quantity quantity = new Quantity();
+        quantity.setQuantity(3.78);
+        quantity.setFirstSubUnit(UnitConversion.Litre);
+        quantity.setSecondSubUnit(UnitConversion.Gallon);
+        double result = quantityMeasurementService.unitConversion(quantity);
+        Assert.assertEquals(1.0,result,0.0);
+    }
+
+    @Test
     public void givenQuantityMeasurement_WhenHundredCelsiusConvertIntoFahrenheit_ThenReturnTwoHundredTwelve() throws QuantityMeasurementException {
         Quantity quantity = new Quantity();
         quantity.setQuantity(100);
